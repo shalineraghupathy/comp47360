d49498fa5fc08219be47605922e9fac33b4c0fb8
sera-v <74683264+sera-v@users.noreply.github.com>
2024-06-18 22:58:35 +0100

    feat: google places integration in search bar

--- parkfinder-client/src/App.tsx
+++ parkfinder-client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/navbar/NavBar";
 import Home from "./components/home/MainContent";
 import SignUp from "./components/signup/SignUp";
@@ -7,13 +8,13 @@ import "bootstrap/dist/css/bootstrap.css";
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
