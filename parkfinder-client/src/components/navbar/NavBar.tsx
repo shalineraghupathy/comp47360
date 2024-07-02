cfe0b8132969f0d74f3741761e883b4bf5b670b4
sera-v <74683264+sera-v@users.noreply.github.com>
2024-07-02 15:18:19 +0100

    minor fix: container sizing

--- parkfinder-client/src/components/navbar/NavBar.tsx
+++ parkfinder-client/src/components/navbar/NavBar.tsx
@@ -27,64 +27,64 @@ const NavBar: React.FC = () => {
   };
 
   return (
-    <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-2">
-      <Navbar
-        className="Navbar"
-        expand="lg"
-        fixed="top"
-        sticky="top"
-        style={{
-          paddingLeft: "1.5rem",
-          paddingRight: "1.5rem",
-          borderBottom: "1px solid #ccc",
-          backgroundColor: "rgba(255, 255, 255, 0.9)",
-          zIndex: 10,
-        }}
-      >
-        <Container fluid>
-          <Navbar.Brand href="#">ParkFinder</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <LinkContainer to="/">
-                <Nav.Link>Home</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/parkmap">
-                <Nav.Link>Park Search</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/amenities">
-                <Nav.Link>Amenities</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/about">
-                <Nav.Link>About</Nav.Link>
-              </LinkContainer>
-              <NavDropdown
-                title={<FaUserCircle size={24} />}
-                id="basic-nav-dropdown"
-                align="end"
-              >
-                {isLoggedIn ? (
-                  <>
-                    <NavDropdown.Header>
-                      Welcome, {userFirstName}
-                    </NavDropdown.Header>
-                    <NavDropdown.Item onClick={handleSignOut}>
-                      Sign Out
-                    </NavDropdown.Item>
-                  </>
-                ) : (
-                  <>
-                    <LinkContainer to="/signin">
-                      <NavDropdown.Item>Sign In</NavDropdown.Item>
-                    </LinkContainer>
-                  </>
-                )}
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+    // <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-2">
+    <Navbar
+      className="Navbar"
+      expand="lg"
+      fixed="top"
+      sticky="top"
+      style={{
+        paddingLeft: "1.5rem",
+        paddingRight: "1.5rem",
+        borderBottom: "1px solid #ccc",
+        backgroundColor: "rgba(255, 255, 255, 0.9)",
+        zIndex: 10,
+      }}
+    >
+      <Container fluid>
+        <Navbar.Brand href="#">ParkFinder</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/parkmap">
+              <Nav.Link>Park Search</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/amenities">
+              <Nav.Link>Amenities</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/about">
+              <Nav.Link>About</Nav.Link>
+            </LinkContainer>
+            <NavDropdown
+              title={<FaUserCircle size={24} />}
+              id="basic-nav-dropdown"
+              align="end"
+            >
+              {isLoggedIn ? (
+                <>
+                  <NavDropdown.Header>
+                    Welcome, {userFirstName}
+                  </NavDropdown.Header>
+                  <NavDropdown.Item onClick={handleSignOut}>
+                    Sign Out
+                  </NavDropdown.Item>
+                </>
+              ) : (
+                <>
+                  <LinkContainer to="/signin">
+                    <NavDropdown.Item>Sign In</NavDropdown.Item>
+                  </LinkContainer>
+                </>
+              )}
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    // </div>
   );
 };
 
