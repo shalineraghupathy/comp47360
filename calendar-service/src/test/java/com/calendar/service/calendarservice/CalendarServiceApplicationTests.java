1efd9968ccad0fc3df37bb2851e36d54086ce41b
Yanwen Yu <146300197+yanwennn@users.noreply.github.com>
2024-07-03 23:50:33 +0100

    updata dependencies

---
+++ calendar-service/src/test/java/com/calendar/service/calendarservice/CalendarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calendar.service.calendarservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
