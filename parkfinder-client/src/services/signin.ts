ce86a26bd293125ee7d0e3912e7a804361e98cad
Shaline Raghupathy <shalineraghupathy02@gmail.com>
2024-07-13 00:13:29 +0100

    modify api baseurl

--- parkfinder-client/src/services/signin.ts
+++ parkfinder-client/src/services/signin.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080/api/v1/auth";
+const API_BASE_URL = "http://34.245.187.188:8080/api/v1/auth";
 
 // Define the request types
 interface UserRegisterRequest {
