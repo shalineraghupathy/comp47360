1fdaa259ccf7dca45a96dddce46a851fe3799716
DAVIDWOOD5 <david.woods3@ucdconnect.ie>
2024-07-24 11:09:22 +0100

    navbar component modification, turning selected page titles green + bold

--- parkfinder-client/src/components/navbar/NavBar.tsx
+++ parkfinder-client/src/components/navbar/NavBar.tsx
@@ -2,15 +2,17 @@ import React, { useEffect } from "react";
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { Link as ScrollLink } from "react-scroll";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { FaUserCircle } from "react-icons/fa";
 import useLocalStorage from "../../hooks/useLocalStorage";
 import logo from "../../assets/NYC PARK FINDER LOGO.svg";
+import './NavBar.css';
 
 const NavBar: React.FC = () => {
   const [token, setToken] = useLocalStorage("token");
   const [userFirstName, setUserFirstName] = useLocalStorage("userFirstName");
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     setToken(localStorage.getItem("token"));
@@ -26,6 +28,10 @@ const NavBar: React.FC = () => {
     setToken(null);
     setUserFirstName(null);
     navigate("/signin");
+  };
+
+  const isActive = (path: string) => {
+    return location.pathname === path ? "nav-link-active" : "";
   };
 
   return (
@@ -57,10 +63,10 @@ const NavBar: React.FC = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <LinkContainer to="/">
-              <Nav.Link>Home</Nav.Link>
+              <Nav.Link className={isActive("/")}>Home</Nav.Link>
             </LinkContainer>
             <LinkContainer to="/parkmap">
-              <Nav.Link>Park Map</Nav.Link>
+              <Nav.Link className={isActive("/parkmap")}>Park Map</Nav.Link>
             </LinkContainer>
             <ScrollLink
               to="natParks"
@@ -69,10 +75,10 @@ const NavBar: React.FC = () => {
               offset={-70}
               style={{ cursor: "pointer" }}
             >
-              <Nav.Link as="span">National Parks</Nav.Link>
+              <Nav.Link as="span" className={isActive("/natParks")}>National Parks</Nav.Link>
             </ScrollLink>
             <LinkContainer to="/eventcalendar">
-              <Nav.Link>Event Calendar</Nav.Link>
+              <Nav.Link className={isActive("/eventcalendar")}>Event Calendar</Nav.Link>
             </LinkContainer>
             <ScrollLink
               to="aboutSection"
@@ -81,7 +87,7 @@ const NavBar: React.FC = () => {
               offset={-70}
               style={{ cursor: "pointer" }}
             >
-              <Nav.Link as="span">About</Nav.Link>
+              <Nav.Link as="span" className={isActive("/aboutSection")}>About</Nav.Link>
             </ScrollLink>
             <NavDropdown
               title={<FaUserCircle size={24} />}
