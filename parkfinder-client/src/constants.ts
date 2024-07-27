4f2de5aa6dcefdfa1540056f1e638e7e62d1b704
Shaline Raghupathy <shalineraghupathy02@gmail.com>
2024-07-27 05:26:19 +0100

    updated deployment changes

--- parkfinder-client/src/constants.ts
+++ parkfinder-client/src/constants.ts
@@ -1,3 +1,3 @@
-export const PROFILE_URL = "http://18.201.132.214:8080";
-export const DATA_URL = "http://18.201.132.214:8082";
-export const CALENDAR_URL = "http://18.201.132.214:8084";
+export const PROFILE_URL = "http://34.240.252.150:8080";
+export const DATA_URL = "http://34.240.252.150:8082";
+export const CALENDAR_URL = "http://34.240.252.150:8084";
