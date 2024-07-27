ec91a56268cc427c15c8e9b93b99cb4017615189
Shaline Raghupathy <shalineraghupathy02@gmail.com>
2024-07-27 05:49:04 +0100

    api endpoints updated

--- parkfinder-client/src/constants.ts
+++ parkfinder-client/src/constants.ts
@@ -1,3 +1,3 @@
 export const PROFILE_URL = "http://34.240.252.150:8080";
-export const DATA_URL = "http://34.240.252.150:8082";
-export const CALENDAR_URL = "http://34.240.252.150:8084";
+export const DATA_URL = "http://34.247.13.235:8082";
+export const CALENDAR_URL = "http://18.201.87.73:8084";
