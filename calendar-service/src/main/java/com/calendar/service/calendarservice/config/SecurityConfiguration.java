2de21e18032de6d0a501fddeb962cb671143d14f
Shaline Raghupathy <shalineraghupathy02@gmail.com>
2024-07-27 06:31:23 +0100

    cors issue fix

---
+++ calendar-service/src/main/java/com/calendar/service/calendarservice/config/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.calendar.service.calendarservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public OpenAPI registrationOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("nycparkfinder API")
+                        .description("park finder webpage API")
+                        .version("1.0"));
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors().and().csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll();
+        return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*"); // Allow all origins
+        config.addAllowedHeader("*"); // Allow all headers
+        config.addAllowedMethod("*"); // Allow all methods
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
